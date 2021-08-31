e1dfe6218dcc2d3ffcb917f71d6d7f594c933a2c
Oscar te Giffel <oscar@tegiffel.com>
2021-08-31 14:17:19 +0200

    refactor: moved test to correct place

---
+++ src/gorountines_test.go
@@ -0,0 +1,27 @@
+package src
+
+import (
+	"fmt"
+	"testing"
+	"time"
+)
+
+func f(from string) {
+	for i := 0; i < 3; i++ {
+		fmt.Println(from, ":", i)
+	}
+}
+
+func TestF(t *testing.T) {
+
+	f("direct")
+
+	go f("goroutine")
+
+	go func(msg string) {
+		fmt.Println(msg)
+	}("going")
+
+	time.Sleep(time.Second)
+	fmt.Println("done")
+}
